37573c574ba25ab0e89de875173de6960ad6823f
Keisuke Kan <9renpoto@gmail.com>
2021-02-13 21:54:58 +0900

    fix: gtag dimentions

--- apps/web/utils/gtag.ts
+++ apps/web/utils/gtag.ts
@@ -5,5 +5,6 @@ export const { GA_TRACKING_ID } = process.env;
 export const pageview = (page_path: string) => {
   window.gtag("config", GA_TRACKING_ID!, {
     page_path,
+    hour: new Date().getHours(),
   });
 };
