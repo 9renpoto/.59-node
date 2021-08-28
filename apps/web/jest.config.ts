bdfdf66f51b884e9a392b174494396d2baa50e9e
Keisuke Kan <9renpoto@gmail.com>
2021-08-28 10:16:10 +0900

    chore: upgrade jest

--- apps/web/jest.config.ts
+++ apps/web/jest.config.ts
@@ -2,6 +2,7 @@ import type { Config } from "@jest/types";
 
 const config: Config.InitialOptions = {
   collectCoverage: true,
+  testEnvironment: "jsdom",
   transform: {
     "\\.[jt]sx?$": "babel-jest",
   },
