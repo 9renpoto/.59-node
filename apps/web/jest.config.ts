b52f04227555f48aff4ed467d0bf25aa7a350776
Keisuke Kan <9renpoto@gmail.com>
2022-03-26 20:31:43 +0900

    fix: snapshot

--- apps/web/jest.config.ts
+++ apps/web/jest.config.ts
@@ -1,11 +1,10 @@
 import type { Config } from "@jest/types";
+import nextJest from "next/jest";
 
+const createJestConfig = nextJest({});
 const config: Config.InitialOptions = {
   collectCoverage: true,
-  testEnvironment: "jsdom",
-  transform: {
-    "\\.[jt]sx?$": "babel-jest",
-  },
+  testEnvironment: "jest-environment-jsdom",
 };
 
-export default config;
+export default createJestConfig(config);
