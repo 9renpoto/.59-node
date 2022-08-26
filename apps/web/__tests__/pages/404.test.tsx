593b31dd2e90319219f39313ff53ba8a18977051
Keisuke Kan <9renpoto@gmail.com>
2022-08-26 20:42:21 +0900

    chore: snapshot

--- apps/web/__tests__/pages/404.test.tsx
+++ apps/web/__tests__/pages/404.test.tsx
@@ -7,14 +7,26 @@ describe("404", () => {
     expect(container).toMatchInlineSnapshot(`
       <div>
         <div
-          style="color: rgb(0, 0, 0); background: rgb(255, 255, 255); font-family: -apple-system, BlinkMacSystemFont, Roboto, \\"Segoe UI\\", \\"Fira Sans\\", Avenir, \\"Helvetica Neue\\", \\"Lucida Grande\\", sans-serif; height: 100vh; text-align: center; display: flex; flex-direction: column; align-items: center; justify-content: center;"
+          style="font-family: -apple-system, BlinkMacSystemFont, Roboto, \\"Segoe UI\\", \\"Fira Sans\\", Avenir, \\"Helvetica Neue\\", \\"Lucida Grande\\", sans-serif; height: 100vh; text-align: center; display: flex; flex-direction: column; align-items: center; justify-content: center;"
         >
           <div>
             <style>
-              body { margin: 0 }
+              
+                      body { margin: 0; color: #000; background: #fff; }
+                      .next-error-h1 {
+                        border-right: 1px solid rgba(0, 0, 0, .3);
+                      }
+
+                      @media (prefers-color-scheme: dark) {
+                        body { color: #fff; background: #000; }
+                        .next-error-h1 {
+                          border-right: 1px solid rgba(255, 255, 255, .3);
+                        }
+                      }
             </style>
             <h1
-              style="display: inline-block; border-right: 1px solid rgba(0, 0, 0,.3); margin: 0px 20px 0px 0px; padding: 10px 23px 10px 0px; font-size: 24px; font-weight: 500; vertical-align: top;"
+              class="next-error-h1"
+              style="display: inline-block; margin: 0px 20px 0px 0px; padding: 0px 23px 0px 0px; font-size: 24px; font-weight: 500; vertical-align: top; line-height: 49px;"
             >
               404
             </h1>
@@ -22,7 +34,7 @@ describe("404", () => {
               style="display: inline-block; text-align: left; line-height: 49px; height: 49px; vertical-align: middle;"
             >
               <h2
-                style="font-size: 14px; font-weight: normal; line-height: inherit; margin: 0px; padding: 0px;"
+                style="font-size: 14px; font-weight: normal; line-height: 49px; margin: 0px; padding: 0px;"
               >
                 This page could not be found
                 .
