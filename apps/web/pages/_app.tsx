eabedb6bec9bb2b211fd402f447a0508160845bb
Keisuke Umeno <9renpoto@gmail.com>
2023-07-25 22:24:35 +0900

    chore: add new words;

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -8,7 +8,7 @@ const App = ({ Component, pageProps }: AppProps) => {
   const router = useRouter();
   useEffect(() => {
     const handleRouteChange = (url: string) => {
-      gtag.pageview(url);
+      gtag.pageView(url);
     };
     router.events.on("routeChangeComplete", handleRouteChange);
     return () => {
