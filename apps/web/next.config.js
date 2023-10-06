72363543e206105c9dddf52fd42520f5158e6ff4
Keisuke Umeno <9renpoto@gmail.com>
2023-10-06 16:42:44 +0900

    feat: use playwright

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,12 +1,7 @@
-const withPWA = require("next-pwa");
-const runtimeCaching = require("next-pwa/cache");
+// @ts-check
+
 const withPreact = require("next-plugin-preact");
-const SentryWebpackPlugin = require("@sentry/webpack-plugin");
 const {
-  NEXT_PUBLIC_SENTRY_DSN: SENTRY_DSN,
-  SENTRY_ORG,
-  SENTRY_PROJECT,
-  SENTRY_AUTH_TOKEN,
   NODE_ENV,
   VERCEL_GITHUB_COMMIT_SHA,
   VERCEL_GITLAB_COMMIT_SHA,
@@ -18,15 +13,10 @@ const COMMIT_SHA = VERCEL_GITHUB_COMMIT_SHA ||
   VERCEL_GITLAB_COMMIT_SHA ||
   VERCEL_BITBUCKET_COMMIT_SHA;
 
-process.env.SENTRY_DSN = SENTRY_DSN;
-const basePath = "";
-
+/**
+ * @type {import('next').NextConfig}
+ */
 const config = {
-  swcMinify: true,
-  pwa: {
-    dest: "public",
-    runtimeCaching,
-  },
   productionBrowserSourceMaps: true,
   env: {
     // Make the COMMIT_SHA available to the client so that Sentry events can be
@@ -35,61 +25,6 @@ const config = {
     NEXT_PUBLIC_COMMIT_SHA: COMMIT_SHA,
     GA_TRACKING_ID,
   },
-  webpack: (config, options) => {
-    // In `pages/_app.js`, Sentry is imported from @sentry/browser. While
-    // @sentry/node will run in a Node.js environment. @sentry/node will use
-    // Node.js-only APIs to catch even more unhandled exceptions.
-    //
-    // This works well when Next.js is SSRing your page on a server with
-    // Node.js, but it is not what we want when your client-side bundle is being
-    // executed by a browser.
-    //
-    // Luckily, Next.js will call this webpack function twice, once for the
-    // server and once for the client. Read more:
-    // https://nextjs.org/docs/api-reference/next.config.js/custom-webpack-config
-    //
-    // So ask Webpack to replace @sentry/node imports with @sentry/browser when
-    // building the browser's bundle
-    if (!options.isServer) {
-      config.resolve.alias["@sentry/node"] = "@sentry/browser";
-    }
-
-    // Define an environment variable so source code can check whether or not
-    // it's running on the server so we can correctly initialize Sentry
-    config.plugins.push(
-      new options.webpack.DefinePlugin({
-        "process.env.NEXT_IS_SERVER": JSON.stringify(
-          options.isServer.toString(),
-        ),
-      }),
-    );
-
-    // When all the Sentry configuration env variables are available/configured
-    // The Sentry webpack plugin gets pushed to the webpack plugins to build
-    // and upload the source maps to sentry.
-    // This is an alternative to manually uploading the source maps
-    // Note: This is disabled in development mode.
-    if (
-      SENTRY_DSN &&
-      SENTRY_ORG &&
-      SENTRY_PROJECT &&
-      SENTRY_AUTH_TOKEN &&
-      COMMIT_SHA &&
-      NODE_ENV === "production"
-    ) {
-      config.plugins.push(
-        new SentryWebpackPlugin({
-          include: ".next",
-          ignore: ["node_modules"],
-          stripPrefix: ["webpack://_N_E/"],
-          urlPrefix: `~${basePath}/_next`,
-          release: COMMIT_SHA,
-        }),
-      );
-    }
-    return config;
-  },
-  basePath,
 };
 
-module.exports = withPWA(withPreact(config));
+module.exports = withPreact(config);
