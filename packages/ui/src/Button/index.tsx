4f3780a1a9002231e50cbbd012cb1c13d8e4ce2d
Keisuke Umeno <9renpoto@gmail.com>
2024-01-03 10:09:17 +0900

    refactor: rename a element

--- packages/ui/src/Button/index.tsx
+++ packages/ui/src/Button/index.tsx
@@ -4,6 +4,7 @@ export default function Button(props: JSX.HTMLAttributes<HTMLButtonElement>) {
   return (
     <button
       {...props}
+      type="button"
       class={`px-3 py-2 bg-white rounded border(gray-500 2) hover:bg-gray-200 active:bg-gray-300 disabled:(opacity-50 cursor-not-allowed) ${
         props.class ?? ""
       }`}
