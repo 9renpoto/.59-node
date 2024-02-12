6b91150f0bd447ca51cea9fde3f60f35aaa0d63a
Keisuke Umeno <9renpoto@gmail.com>
2024-02-12 17:13:32 +0900

    fix: remove astro

--- apps/static/astro.config.mjs
+++
@@ -1,9 +0,0 @@
-import preact from "@astrojs/preact";
-import { defineConfig } from "astro/config";
-
-/**
- * @type {import('astro/types').Config}
- */
-export default defineConfig({
-  integrations: [preact()],
-});
