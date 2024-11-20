29d6a592a31b2dd2e048e64407aad2021e4a4250
Keisuke Umeno <9renpoto@gmail.com>
2024-11-21 08:52:46 +0900

    chore(apps): remove web base;

--- apps/web/e2e/index.spec.ts
+++
@@ -1,7 +0,0 @@
-import { expect, test } from "@playwright/test";
-
-test("should navigate to the about page", async ({ page }) => {
-  await page.goto("/");
-  await page.getByText("About").click();
-  await expect(page).toHaveURL("/about");
-});
--- apps/web/pages/404.tsx
+++
@@ -1,5 +0,0 @@
-import ErrorPage from "next/error";
-
-export default function NotFound() {
-  return <ErrorPage statusCode={404} />;
-}
