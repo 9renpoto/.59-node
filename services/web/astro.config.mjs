cd9303c24cb9b1e8a3d18b72eea81e081b08c3fa
Keisuke Umeno <9renpoto@gmail.com>
2025-09-12 16:53:36 +0900

    chore: biome . --write

--- services/web/astro.config.mjs
+++ services/web/astro.config.mjs
@@ -1,6 +1,6 @@
 import preact from "@astrojs/preact";
-import { defineConfig } from "astro/config";
 import codecovAstroPlugin from "@codecov/astro-plugin";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
